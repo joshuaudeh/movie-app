acf8c2340705e0821deb6a85e90d30c438d71677
root <root@LAPTOP-D31HAE9N.localdomain>
2022-03-22 18:15:56 +0100

    resized images

--- src/features/Movie.js
+++ src/features/Movie.js
@@ -2,19 +2,20 @@ import React from "react";
 
 const image_url = "https://image.tmdb.org/t/p/w500";
 
+const setVoteClass = (vote) => {
+  if (vote >= 8) {
+    return "green";
+  } else if (vote >= 6) {
+    return "orange";
+  } else {
+    return "red";
+  }
+};
+
 function Movie({ title, name, overview, poster_path, vote_average }) {
   const img =
     "https://thumbs.dreamstime.com/b/movie-slate-film-reel-wood-clapper-wooden-backgorund-36502412.jpg";
 
-  const setVoteClass = (vote) => {
-    if (vote >= 8) {
-      return "green";
-    } else if (vote >= 6) {
-      return "orange";
-    } else {
-      return "red";
-    }
-  };
   return (
     <div className="movie">
       <img src={poster_path ? image_url + poster_path : img} alt={title} />
